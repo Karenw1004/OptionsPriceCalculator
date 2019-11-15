bbd7e45714efe6e420392e4a3e794b42d8cad698
Karenw1004 <karenw0103@gmail.com>
2019-11-15 13:37:28 -0600

    Test get route and WORKING

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
-from flask import Flask, render_template,jsonify,Blueprint
+from flask import Flask, render_template,jsonify,Blueprint,request
 from flask_cors import CORS, cross_origin
 import os
 from dotenv import load_dotenv
-
+from flask_pymongo import PyMongo
 app = Flask(__name__)
 load_dotenv(verbose=True)
 #need SECRET_KEY to encrypt cookies and save send them to the browser
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 # need CORS(cross origin requests) 
 # Explanation can be found at documentation
 CORS(app)
 
-#Register the blueprints(get and post)
+#TODO:Register the blueprints(get and post)
+mongo = PyMongo(app,retryWrites=False)
+@app.route("/all",methods=["GET"])
+def all():
+    # #collection name
+    price = mongo.db.price
+    price_all = price.find({})
+    data = []
+    for element in price_all:
+        print("here")
+        element['_id'] = str(element['_id'])
+        data.append(element)
+    return jsonify({ "data" : data })
 
 @app.route("/")
 def index():
--- routes/get.py
+++ routes/get.py
@@ -1,3 +1,4 @@
-
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
+
+from flask import Flask,jsonify,request,Blueprint
